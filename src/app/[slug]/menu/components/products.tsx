36bd88521eb9c59d8da52993ad4a7c3d753b62c5
Leonardo Palma <leonardoc.palma@outlook.com>
2025-02-23 19:04:15 -0300

    feat: add

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -7,11 +7,12 @@ interface ProductsProps {
 }
 
 const Products = ({ products }: ProductsProps) => {
-    return <div className="space-y-3">
-        {products.map(product => (
+    return (
+    <div className="space-y-3">
+        {products.map((product) => (
             <Link key={product.id} href="/" className="flex items-center justify-between gap-10 py-3">
                 <div>
-                    <h3 className="text-sm font-medium">
+                <h3 className="text-sm font-medium">
                     {product.name}
                     </h3>
                     <p className="line-clamp-2 text sm text muted-foreground">
@@ -20,10 +21,12 @@ const Products = ({ products }: ProductsProps) => {
                     <p className="pt-3 text-sm font-semibold">
                         {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(product.price)}
                     </p>
+                    
                 </div>
             </Link>
         ))}
     </div>
+    )   
 }
  
 export default Products;
