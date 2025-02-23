06fd99098cce62eb4953e5c34a3d942b79b0803f
Leonardo Palma <leonardoc.palma@outlook.com>
2025-02-23 19:01:55 -0300

    feat: Add Metade da aula 3

---
+++ src/app/[slug]/menu/components/categories.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { Prisma } from "@prisma/client";
+import { ClockIcon } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+
+import Products from "./products";
+
+interface RestaurantCategoriesProps {
+  restaurant: Prisma.RestaurantGetPayload<{
+    include: {
+      menuCategories: {
+        include: {
+          products: true;
+        };
+      };
+    };
+  }>;
+}
+
+type MenuCategoryWithProducts = Prisma.MenuCategoryGetPayload<{
+  include: {
+    products: true;
+  };
+}>;
+
+const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
+  const [selectCategory, setSelectCategory] =
+    useState<MenuCategoryWithProducts>(restaurant.menuCategories[0]);
+
+    
+
+  const handleCategoryClick = (category: MenuCategoryWithProducts) => {
+    setSelectCategory(category);
+  };
+
+  const getCategoryButtonVariants = (category: MenuCategoryWithProducts) => {
+    return selectCategory.id === category.id ? "default" : "secondary";
+  };
+
+  return (
+    <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl border bg-white">
+      <div className="p-5">
+        <div className="flex items-center gap-3">
+          <Image
+            src={restaurant.avatarImageUrl}
+            alt={restaurant.name}
+            height={60}
+            width={60}
+          />
+          <div>
+            <h2 className="text-xl font-semibold">{restaurant.name}</h2>
+            <p className="text-xs opacity-55">{restaurant.description}</p>
+          </div>
+        </div>
+        <div className="mt-3 flex items-center gap-1 text-xs text-green-500">
+          <ClockIcon size={15} />
+          <p>Aberto!</p>
+        </div>
+      </div>
+
+      <ScrollArea className="w-full">
+        <div className="flex w-max space-x-4 p-4 pt-0">
+          {restaurant.menuCategories.map((category) => (
+            <Button
+              onClick={() => handleCategoryClick(category)}
+              key={category.id}
+              variant={getCategoryButtonVariants(category)}
+              size="sm"
+              className="rounded-full"
+            >
+              {category.name}
+            </Button>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+
+      <Products products={selectCategory.products} />
+    </div>
+  );
+};
+
+export default RestaurantCategories;
---
+++ src/app/[slug]/menu/components/products.tsx
@@ -0,0 +1,29 @@
+import { Product } from "@prisma/client";
+import Link from "next/link";
+
+
+interface ProductsProps {
+    products: Product[]
+}
+
+const Products = ({ products }: ProductsProps) => {
+    return <div className="space-y-3">
+        {products.map(product => (
+            <Link key={product.id} href="/" className="flex items-center justify-between gap-10 py-3">
+                <div>
+                    <h3 className="text-sm font-medium">
+                    {product.name}
+                    </h3>
+                    <p className="line-clamp-2 text sm text muted-foreground">
+                    {product.description}
+                    </p>
+                    <p className="pt-3 text-sm font-semibold">
+                        {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(product.price)}
+                    </p>
+                </div>
+            </Link>
+        ))}
+    </div>
+}
+ 
+export default Products;
